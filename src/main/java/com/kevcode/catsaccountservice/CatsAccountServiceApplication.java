c333ebc3c691ba32f75b7a5a89d514f900515c28
Kevin Pontón <kevin.ponton@devzeros.com>
2022-03-25 17:36:48 -0500

    first commit

---
+++ src/main/java/com/kevcode/catsaccountservice/CatsAccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kevcode.catsaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatsAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatsAccountServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevcode/catsaccountservice/CatsAccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevcode.catsaccountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatsAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
